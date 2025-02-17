fcb8ed157efce0157417c98e07fe09b331d318ee
Ryan4527 <gu8gi8@gmail.com>
2025-02-17 21:53:12 +0900

    main

---
+++ 20241222MFDSQA.py
@@ -0,0 +1,61 @@
+# TF-IDF 적용
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
+import streamlit as st
+from datetime import datetime
+
+
+data_file = '2024MFDSQNA.csv'
+log_file = 'Q_search_log.csv'
+
+st.set_page_config(page_title="2024 식약처 자연어 QNA 검색", layout="wide")
+st.title("2024 식약처 자연어 QNA 검색")
+st.text("입력된 문장으로 가장 유사한 질의응답을 찾아줍니다.")
+
+df = pd.read_csv(data_file)
+
+new_post = st.text_input("검색할 질의 문장을 입력하세요.")
+# new_post = ['액제 이화학적동등성 시험 선정']
+
+def save_log(new_post, search_time):
+    log_data = pd.DataFrame([[new_post, search_time]], columns=["입력 문장", "입력 시간"])
+
+    # 기존 로그가 있으면 불러오기
+    try:
+        existing_logs = pd.read_csv(log_file)
+        log_data = pd.concat([existing_logs, log_data], ignore_index=True)
+    except FileNotFoundError:
+        pass
+
+    log_data.to_csv(log_file, index=False, encoding="utf-8-sig")
+
+if new_post:
+    search_time = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+    save_log(new_post, search_time)
+
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(df['QNA'].tolist() + [new_post])
+    cosine_sim = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1])
+    similarity_scores = cosine_sim[0]
+    top_indices = np.argsort(similarity_scores)[::-1][:5]
+    top_scores = similarity_scores[top_indices]
+
+    # most_similar_index = np.argmax(similarity_scores)
+    # most_similar_score = similarity_scores[most_similar_index]
+
+    # 결과 출력
+    st.subheader("가장 유사한 QNA TOP 5")
+    for i, index in enumerate(top_indices):
+        st.write(f"**{i+1}. {df.iloc[index]['QNA']}** (유사도 점수: {top_scores[i]:.2f})")
+
+    st.dataframe(pd.DataFrame({
+        "순위": range(1, 6),
+        "유사도 점수": top_scores,
+        "QNA": df.iloc[top_indices]["QNA"].values
+    }))
+
+
+# print(f"가장 유사한 문장: {df.iloc[most_similar_index]['QNA']}")
+# print(f"유사도 점수: {most_similar_score:.2f}")
